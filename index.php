c4f0967cf2de41f045925e719d8a2039fac48baa
JeremyTsimplon <jeremscorpion@gmail.com>
2016-01-28 14:48:09 +0100

    challenge

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
+        <title>Challenge Simplon PhP</title>
+    </head>
+
+
+    <body>
+        <?php
+        // Afficher les erreurs à l'écran
+        ini_set('display_errors', 1);
+        // Enregistrer les erreurs dans un fichier de log
+        ini_set('log_errors', 1);
+        // Nom du fichier qui enregistre les logs (attention aux droits à l'écriture)
+        ini_set('error_log', dirname(__file__) . '/log_error_php.txt');
+        // Afficher les erreurs et les avertissements
+        ?>
+        
+    </body>
+
+
+</html>
